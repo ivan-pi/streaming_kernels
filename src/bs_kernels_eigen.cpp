6af4f752c811321279420355d6c80c0b49ee7777
Ivan Pribec <ivan.pribec@gmail.com>
2025-07-08 18:13:26 +0200

    Add Eigen++ primitives

---
+++ src/bs_kernels_eigen.cpp
@@ -0,0 +1,45 @@
+
+#define EIGEN_NO_DEBUG
+
+#include <Eigen/Dense>
+
+extern "C" {
+
+// Subroutine: b = a
+void bs1_(const int* n, const double* a, double* b) {
+    Eigen::Map<const Eigen::VectorXd> va(a, *n);
+    Eigen::Map<Eigen::VectorXd> vb(b, *n);
+    vb = va;
+}
+
+// Subroutine: y = alpha*x + beta*y
+void bs2_(const int* n, const double* alpha, const double* x,
+          const double* beta, double* y) {
+    Eigen::Map<const Eigen::VectorXd> vx(x, *n);
+    Eigen::Map<Eigen::VectorXd> vy(y, *n);
+    vy = (*alpha) * vx + (*beta) * vy;
+}
+
+// Subroutine: nrm = dot_product(a,a)
+void bs3_(const int* n, const double* a, double* nrm) {
+    Eigen::Map<const Eigen::VectorXd> va(a, *n);
+    *nrm = va.squaredNorm();  // Equivalent to dot(a,a)
+}
+
+// Subroutine: nrm = dot_product(x,y)
+void bs4_(const int* n, const double* x, const double* y, double* nrm) {
+    Eigen::Map<const Eigen::VectorXd> vx(x, *n);
+    Eigen::Map<const Eigen::VectorXd> vy(y, *n);
+    *nrm = vx.dot(vy);
+}
+
+// Subroutine: r = r + alpha*Ap; rdr = dot_product(r,r)
+void bs5_(const int* n, const double* Ap, const double* alpha,
+          double* r, double* rdr) {
+    Eigen::Map<const Eigen::VectorXd> vAp(Ap, *n);
+    Eigen::Map<Eigen::VectorXd> vr(r, *n);
+    vr += (*alpha) * vAp;
+    *rdr = vr.squaredNorm();
+}
+
+} // extern "C"
